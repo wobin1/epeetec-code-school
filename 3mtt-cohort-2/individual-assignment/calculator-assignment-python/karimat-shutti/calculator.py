be38ead77e7f9e336174327af345777438ae9192
Karimat22 <kattylove852@gmail.com>
2024-09-28 15:38:14 +0100

    assignment on calculator

--- 3mtt-cohort-2/individual-assignment/calculator-assignment-python/karimat-shutti/calculator.py
+++ 3mtt-cohort-2/individual-assignment/calculator-assignment-python/karimat-shutti/calculator.py
@@ -12,7 +12,7 @@ def multiply(num1, num2):
 
 def divide(num1, num2):
     if num2 == 0:
-        return "Error: Division by zero is not allowed!!"
+        return "Error: Division by zero is not allowed!!!"
     else:
         return num1 / num2
 
